69ca0131c0b204e0fc31fc4e215c319cce6cf778
Rodrigo Machado Mena <rodrigo.mena8632@gmail.com>
2024-03-16 20:23:54 -0300

    Fix: Browser support + fixing flexbox gap in Safari

--- js/script.js
+++ js/script.js
@@ -85,7 +85,6 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
